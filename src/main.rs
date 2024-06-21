b5ec25ee0314e5e5beca63f76e60e15fa6097dc7
Luke Scanlan <luke@lukescanlan.com>
2024-06-21 21:46:37 +0100

    feat: created initial bird with gravity logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+const GRAVITY: f32 = -9.8;
+const RADIUS: f32 = 30.0;
+
+struct Bird {
+    height: f32,
+    x_position: f32,
+    velocity: f32,
+}
+
+impl Bird {
+    pub fn new() -> Bird {
+        Bird {
+            height: screen_height() / 2.0,
+            x_position: screen_width() * 0.25,
+            velocity: 0.0,
+        }
+    }
+
+    pub fn draw(&self) {
+        draw_circle(self.x_position, self.height, RADIUS, PURPLE);
+    }
+
+    pub fn update(&mut self) {
+        self.height += self.velocity;
+        self.velocity -= get_frame_time() * GRAVITY;
+    }
+}
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    let mut bird = Bird::new();
+
+    loop {
+        clear_background(WHITE);
+        draw_text("MOLLY BIRD!", 20.0, 20.0, 30.0, DARKGRAY);
+
+        bird.draw();
+        bird.update();
+
+        next_frame().await
+    }
 }
