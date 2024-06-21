720710af95289c051cf615250948acacc4d29493
Luke Scanlan <luke@lukescanlan.com>
2024-06-21 19:31:11 +0100

    chore: add czg config

---
+++ cz.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  scopes: ["ui", "logic", "build"],
+  allowEmptyScopes: true,
+  allowCustomScopes: false,
+  emptyScopesAlias: "empty",
+  markBreakingChangeMode: false,
+  breaklineNumber: 80,
+  breaklineChar: "|",
+  skipQuestions: ["footer", "footerPrefix", "confirmCommit"],
+  allowEmptyIssuePrefix: true,
+  confirmColorize: true,
+  maxHeaderLength: 80,
+  scopeOverrides: undefined,
+  defaultBody: "",
+  defaultIssues: "",
+  defaultScope: "",
+  defaultSubject: "",
+};
