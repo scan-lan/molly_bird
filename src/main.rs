71e7f4e03249db5413275ac7058114e04c655844
Luke Scanlan <luke@lukescanlan.com>
2024-06-21 23:23:25 +0100

    feat: add initial obstacle

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use macroquad::prelude::*;
+use std::collections::VecDeque;
+
+use macroquad::{prelude::*, rand};
 
 const GRAVITY: f32 = 9.8;
-const RADIUS: f32 = 30.0;
+const RADIUS: f32 = 25.0;
 const JUMP_VELOCITY: f32 = -4.5;
+const GAP_SIZE: f32 = (RADIUS + 7.) * 2.;
+const OBSTACLE_WIDTH: f32 = 30.0;
 
 struct Bird {
     height: f32,
@@ -29,7 +33,7 @@ impl Bird {
         self.height += self.velocity;
         self.velocity += get_frame_time() * GRAVITY;
 
-        if self.height > screen_height() {
+        if self.height > screen_height() && self.velocity < 0. {
             self.reset();
         }
     }
@@ -44,9 +48,43 @@ impl Bird {
     }
 }
 
+struct Obstacle {
+    x_position: f32,
+    gap_height: f32,
+}
+
+impl Obstacle {
+    pub fn new() -> Self {
+        Obstacle {
+            x_position: screen_width() - 100.0,
+            gap_height: rand::gen_range(40.0, screen_height() - 40.0),
+        }
+    }
+
+    pub fn draw(&self) {
+        let t_rect_h = self.gap_height - (GAP_SIZE / 2.0);
+        // Top rectangle
+        draw_rectangle(self.x_position, 0.0, OBSTACLE_WIDTH, t_rect_h, DARKGRAY);
+
+        let b_rect_start = self.gap_height + GAP_SIZE / 2.0;
+        let b_rect_h = screen_height() - (self.gap_height + GAP_SIZE / 2.0);
+
+        // Bottom rectangle
+        draw_rectangle(
+            self.x_position,
+            b_rect_start,
+            OBSTACLE_WIDTH,
+            b_rect_h,
+            DARKGRAY,
+        );
+    }
+}
+
 #[macroquad::main("Molly Bird")]
 async fn main() {
     let mut bird = Bird::new();
+    let mut obstacles = VecDeque::new();
+    obstacles.push_back(Obstacle::new());
 
     loop {
         clear_background(WHITE);
@@ -58,6 +96,11 @@ async fn main() {
 
         bird.update();
         bird.draw();
+
+        obstacles.iter().for_each(|obstacle| {
+            obstacle.draw();
+            // obstacle.update();
+        });
 
         next_frame().await
     }
